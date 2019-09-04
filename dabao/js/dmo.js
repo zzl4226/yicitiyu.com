c8b72b88a55491fe2f5d9788d01ea1fac5e46c9f
zzl4226 <zzl4226@sina.cn>
2019-09-04 16:05:30 +0800

    update 16:05

---
+++ dabao/js/dmo.js
@@ -0,0 +1,15 @@
+(function($) {
+    "use strict";
+
+   
+    /* ==============================================
+       LOADER -->
+        =============================================== */
+
+    $(window).load(function() {
+        $("#preloader").on(500).fadeOut();
+        $(".preloader").on(600).fadeOut("slow");
+    });
+
+  
+    });
