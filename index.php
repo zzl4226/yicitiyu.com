2134ce937217b62d4739fe9912b720c74a441b66
zzl4226 <zzl4226@sina.cn>
2019-10-10 16:13:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="zh-cmn-Hans">
+<head>
+	<title>一次体育健身，超大恒温泳池，刷新您身边健身房级别，一个专业健身减肥瘦身CLUB。帮助你设置科学健身课程，制定减肥方法，配置健康饮食，让你轻松减肥，快速塑形，一起嗨运动！</title>	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<meta name="keywords" content="减肥瘦身,健身塑形,增肌课程计划,锻炼肌肉,运动">
+	<meta name="description" content="一次体育健身网，是一个专业健身减肥瘦身网站。帮助你设置科学健身课程，制定减肥方法，配置健康饮食，让你轻松减肥，快速塑形，一起嗨运动！">
+	<!-- start video.js -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.3.0/video-js.min.css" rel="stylesheet">
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.3.0/video.min.js"></script>
+	<!-- end video.js -->
+	<meta charset="utf-8">
+	<meta name="keywords" content="" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+
+	</script>
+	<!--// Meta tag Keywords -->
+	
+	<!-- css files -->
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<!-- //css files -->
+	
+	<!--web font-->
+	<link href="http://fonts.googleapis.com/css?family=Hind:300,400,500,600,700&amp;subset=devanagari,latin-ext" rel="stylesheet">
+	<!--//web font-->
+
+</head>
+
+<body>
+
+<!-- header -->
+<header>
+	<div class="container">
+		<div class="header d-lg-flex justify-content-between align-items-center">
+			<div class="header-agile">
+				<h1>
+					<a class="navbar-brand logo" href="index.html">
+						<img src="images/logow.png">
+					</a>
+				</h1>
+			</div>
+
+		</div>
+	</div>
+</header>
+<!-- //header -->
+<!-- banner -->
+<div class="banner_w3lspvt" id="home">
+	<div class="csslider infinity" id="slider1">
+		<input type="radio" name="slides" id="slides_1"/>
+		<input type="radio" name="slides" id="slides_2" checked="checked"/>
+		<input type="radio" name="slides" id="slides_3"/>
+		<input type="radio" name="slides" id="slides_4"/>
+
+		<ul class="banner_slide_bg">
+			<li>
+				<div class="slider-info bg1"></div>
+			</li>
+			<li>
+				<div class="slider-info bg2"></div>
+			</li>
+			<li>
+				<div class="slider-info bg3"></div>
+			</li>
+			<li>
+				<div class="slider-info bg4"></div>
+			</li>
+		</ul>
+		<div class="navigation"> 
+			<div>
+			  <label for="slides_1"></label>
+			  <label for="slides_2"></label>
+			  <label for="slides_3"></label>
+			  <label for="slides_4"></label>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- how we work -->
+<section class="how py-5" id="how">
+	<div class="container">
+		<div class="inner-grid py-5">
+			<h3 class="heading text-center mb-3">选择one time fitness</h3>
+			<!-- <img src="images/banner1.jpg"> -->
+			 <p align="center">
+				<video width="300" height="autoplay" controls="controls" autoplay="autoplay" >
+					<source src="http://vjs.zencdn.net/v/oceans.mp4" />
+				</video>
+			</p>
+		</div>
+	</div>
+</section>
+<!-- //how we work -->
+
+<!-- improvements -->
+<section class="stats py-5">
+	<div class="container py-md-5">
+		<div class="heading">
+			<h3 class="heading text-center">One Time Fitness 您的健康管家</h3>
+			<p class="my-3 head text-center"> 
+				<span>免费制定您的个人计划</span><hr>
+				<span>最专业的健身动作库：超过2000+动作</span><hr>
+				<span>最全面的健身计划库：超过600+计划</span>
+			</p>
+		</div>
+		<div class="row mt-5"> 
+			<div class="col-lg-3 col-sm-6">
+				<div class="bg-white p-4">
+					<h5>腹肌雕刻</h5>
+					<h4 class="mt-2 mb-sm-3">200+</h4>
+					<p> 							
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1465897090762_am__len67153.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="腹肌雕刻 - 零基础" title="腹肌雕刻 - 零基础">
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-sm-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>零基础全身激活</h5>
+					<h4 class="mt-2 mb-sm-3">2,800</h4>
+					<p> 
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1464157751586_am__len99160.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="零基础全身激活" title="零基础全身激活">
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>腹肌雕刻</h5>
+					<h4 class="mt-2 mb-sm-3">390+</h4>
+					<p> 										
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1464146137731_am__len52553.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="腹肌雕刻 - 初级" title="腹肌雕刻 - 初级">
+ 					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>HIIT全身脂肪燃动</h5>
+					<h4 class="mt-2 mb-sm-3">2,450</h4>
+					<p>
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1465896900278_am__len116985.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="HIIT全身脂肪燃动-零基础" title="HIIT全身脂肪燃动-零基础">
+					</p>
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-sm-6">
+				<div class="bg-white p-4">
+					<h5>脂肪燃烧者</h5>
+					<h4 class="mt-2 mb-sm-3">200+</h4>
+					<p> 
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1464157607543_am__len94440.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="脂肪燃烧者 - 初级" title="脂肪燃烧者 - 初级">
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-sm-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>4分钟Tabata全身燃动</h5>
+					<h4 class="mt-2 mb-sm-3">2,800</h4>
+					<p>
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1471856317913_am__len3052268.png?imageview/4/0/w/388/h/170/blur/0.98" alt="4分钟Tabata全身燃动-零基础" title="4分钟Tabata全身燃动-零基础">
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>驼背改善</h5>
+					<h4 class="mt-2 mb-sm-3">390+</h4>
+					<p>
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1467883439500_am__len78587.jpg?imageview/4/0/w/388/h/170/blur/0.98" alt="驼背改善" title="驼背改善">
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6 mt-lg-0 mt-3">
+				<div class="bg-white p-4">
+					<h5>维密-美腿训练</h5>
+					<h4 class="mt-2 mb-sm-3">2,450</h4>
+					<p>
+						<img src="https://w2.dwstatic.com/yy/ojiastoreimage/1471856260836_am__len1032504.png?imageview/4/0/w/388/h/170/blur/0.98" alt="维密-美腿训练" title="维密-美腿训练">
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //improvements -->
+
+<!-- subscribe -->
+<section class="subscribe">
+	<div class="subscribe-overlay py-5">
+		<div class="container py-md-5">
+			<div class="row subscribe-grids">
+				<div class="col-lg-6">
+					<p class="mb-4">
+						<ul style="color: white;">
+							<li>器械动作</li>
+							<li>哑铃 杠铃 壶铃 弹力带 TRX绳 战绳</li>
+							<li>药球 瑞士球 波速球 泡沫轴 龙门架</li>
+							<li>推肩器 倒蹬机 哈克机 腿弯举机</li>
+							<li>推胸机 悍马机 夹胸器 划船机 下拉机</li>
+							<li>单杠 双杠 仰卧板 罗马椅 牧师凳</li>
+							<li>史密斯机 提踵机 地雷架 腿屈伸机</li>
+						</ul>
+					</p>
+
+					<p class="mb-4">
+						<dl style="color: white;">
+							<dt>新手课程</dt>
+							<dt>新手专题 | 郑多燕减肥操</dt>
+							<dt>P4P健身 | 10分钟健身</dt>
+							<dt>Slim in 6 | New Slim in 6</dt>
+							<dt> Power 90 | P90 2014</dt>
+							<dt>翘臀圣经 | Rockin Body</dt>
+						</dl>
+					</p>
+					<h5><span class="fa mr-2 fa-cogs" aria-hidden="true"></span> Posuere Curae, Nulla mollis dapibus.</h5>
+					<h5><span class="fa mr-2 fa-tty" aria-hidden="true"></span> Faucibus orci luctuset ipsum.</h5>
+					<h5><span class="fa mr-1 fa-cubes" aria-hidden="true"></span><span id="ecm">一次体育会籍管理</span></h5>
+				</div>
+				<div class="col-lg-6 mt-lg-0 mt-sm-5 mt-4">
+					<h3 class="heading">加入我们</h3>
+					<p class="mb-4">Get involved and stay up to date with what's happening.</p>
+					<form action="#" method="post" class="mt-4">  
+						<input type="text" name="Name" placeholder="姓名" required="">
+						<input  type="text"  id="phone" maxlength="11" class="mr-0" placeholder="电话" onkeyup="this.value=this.value.replace(/\D/g,'')" />
+						<button type="submit" class="btn mt-2"> 加入 </button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- subscribe -->
+
+<!-- footer -->
+<footer class="py-sm-5 py-4">
+	<div class="container py-md-3">
+		<div class="footer-grids text-center">
+			<h2 class="logo"><a href="index.html"><img style="width:8%" src="images/footlogo.png"> 一次体育</a></h2>
+		</div>
+		<!-- to top -->
+		<div class="top-icon text-center mt-2">
+			<a href="#home" class="move-top text-center"><span class="fa fa-angle-double-up" aria-hidden="true"></span> Go to top</a>
+		</div>
+		<!-- //to top -->
+		<div class="middle mt-3">
+			<ul class="social mb-4">
+				<li><a href="#"><span class="fa fa-weixin fa-2x"></span></a></li>
+				<li><a href="#"><span class="fa fa-weibo fa-2x"></span></a></li>
+				<li><a href="#"><span class="fa fa-qq fa-2x"></span></a></li>
+				<li><a href="#"><span class="fa fa-envelope fa-2x"></span></a></li>
+				<!--   -->
+			</ul>
+			<p><a href="http://www.beian.miit.gov.cn">渝ICP备19011407号-1</a> - Collect from <a href="http://www.yicitiyu.com/" title="一次体育" target="_blank">一次体育</a></p>
+			<p>地址： <a href="https://surl.amap.com/yHgKJln7RS" target="_blank">重庆市南岸区学府大道51号力豪健身(一次体育体验店)</a></p>
+			<p>电话：023-86801148</p>
+		</div>
+	</div>
+</footer>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#ecm").click(function(){
+			$(location).attr("href","http://ys.yicitiyu.com");
+		});
+	});
+</script>
+
+</body>
+</html>
